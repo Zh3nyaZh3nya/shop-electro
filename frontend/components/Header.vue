2907534a35f9cbd8587c62057850c3824a1cb9d4
Evgeniy Golovkin <evgeniq.golovkin@gmail.com>
2024-12-07 20:16:53 +0500

    init

---
+++ frontend/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { useLocalePath } from "#i18n"
+
+const localePath = useLocalePath()
+
+const menu = ref([
+  {
+    title: 'menu_title_1',
+    link: '/search/resume',
+  },
+  {
+    title: 'menu_title_2',
+    link: '/search/vacancy',
+  },
+  {
+    title: '',
+    link: '',
+  },
+])
+</script>
+
+<template>
+  <v-toolbar color="transparent" class="py-2">
+    <v-container>
+
+    </v-container>
+  </v-toolbar>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-app>
+    <Header />
+    <NuxtPage />
+  </v-app>
+</template>
+
+<style scoped>
+
+</style>
