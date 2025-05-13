91ebf2c7ab226df1dea3929f77dee5de142a868f
Evgeniy Golovkin <evgeniq.golovkin@gmail.com>
2025-05-14 01:45:10 +0500

    feat: add adaptive

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -31,61 +31,69 @@ const menuSecond: { title: string, link: string }[] = [
 
 <template>
   <v-footer color="grey-light-2">
-    <v-container class="py-10">
+    <v-container class="px-10 pt-10 py-16 py-md-10 ">
       <v-row>
         <v-col
+            sm="2"
+            md="1"
             lg="1"
+            class="d-none d-sm-block"
         >
           <nuxt-link>
             <v-img src="/logo.png" height="80px" />
           </nuxt-link>
         </v-col>
         <v-col
+            sm="10"
+            md="11"
             lg="4"
         >
-          <div class="d-flex ga-8">
-            <div class="d-flex flex-column">
-              <ul>
-                <li
-                    v-for="link in menuFirst"
-                    :key="link.title"
-                    class="text-h6 mb-2 font-weight-regular"
-                >
-                  <nuxt-link :to="link.link" class="text-secondary link-hover">
-                    {{ link.title }}
-                  </nuxt-link>
-                </li>
-              </ul>
+          <div class="d-flex justify-sm-space-between align-sm-center align-lg-start flex-column flex-sm-row flex-lg-column">
+            <div class="d-flex flex-column flex-sm-row ga-0 ga-sm-8">
+              <div class="d-flex flex-column">
+                <ul>
+                  <li
+                      v-for="link in menuFirst"
+                      :key="link.title"
+                      class="text-h6 mb-2 font-weight-regular"
+                  >
+                    <nuxt-link :to="link.link" class="text-secondary link-hover">
+                      {{ link.title }}
+                    </nuxt-link>
+                  </li>
+                </ul>
+              </div>
+              <div class="d-flex flex-column">
+                <ul>
+                  <li
+                      v-for="link in menuSecond"
+                      :key="link.title"
+                      class="text-h6 mb-2 font-weight-regular"
+                  >
+                    <nuxt-link :to="link.link" class="text-secondary link-hover">
+                      {{ link.title }}
+                    </nuxt-link>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div class="d-flex flex-column">
-              <ul>
-                <li
-                  v-for="link in menuSecond"
-                  :key="link.title"
-                  class="text-h6 mb-2 font-weight-regular"
-                >
-                  <nuxt-link :to="link.link" class="text-secondary link-hover">
-                    {{ link.title }}
-                  </nuxt-link>
-                </li>
-              </ul>
+            <div class="d-flex ga-4 mt-4">
+              <a href="https://t.me/Zh3nya_qwe" target="blank" rel="noreferrer noopener" class="d-flex align-center justify-center icon-social tg">
+                <svg width="24px" height="24px" viewBox="0 0 48 48" id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" fill="#fff">
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path class="cls-1" d="M40.83,8.48c1.14,0,2,1,1.54,2.86l-5.58,26.3c-.39,1.87-1.52,2.32-3.08,1.45L20.4,29.26a.4.4,0,0,1,0-.65L35.77,14.73c.7-.62-.15-.92-1.07-.36L15.41,26.54a.46.46,0,0,1-.4.05L6.82,24C5,23.47,5,22.22,7.23,21.33L40,8.69a2.16,2.16,0,0,1,.83-.21Z">
+                    </path>
+                  </g>
+                </svg>
+              </a>
+              <a href="https://www.instagram.com/evg3niy_s0s0viashv1l1/" target="blank" rel="noreferrer noopener" class="d-flex align-center justify-center icon-social insta">
+                <v-icon icon="mdi-instagram" size="20px" color="white" />
+              </a>
             </div>
           </div>
-          <div class="d-flex ga-4 mt-4">
-            <a href="https://t.me/Zh3nya_qwe" target="blank" rel="noreferrer noopener" class="d-flex align-center justify-center icon-social tg">
-              <svg width="24px" height="24px" viewBox="0 0 48 48" id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" fill="#fff">
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                <g id="SVGRepo_iconCarrier">
-                  <path class="cls-1" d="M40.83,8.48c1.14,0,2,1,1.54,2.86l-5.58,26.3c-.39,1.87-1.52,2.32-3.08,1.45L20.4,29.26a.4.4,0,0,1,0-.65L35.77,14.73c.7-.62-.15-.92-1.07-.36L15.41,26.54a.46.46,0,0,1-.4.05L6.82,24C5,23.47,5,22.22,7.23,21.33L40,8.69a2.16,2.16,0,0,1,.83-.21Z">
-                  </path>
-                </g>
-              </svg>
-            </a>
-            <a href="https://www.instagram.com/evg3niy_s0s0viashv1l1/" target="blank" rel="noreferrer noopener" class="d-flex align-center justify-center icon-social insta">
-              <v-icon icon="mdi-instagram" size="20px" color="white" />
-            </a>
-          </div>
+
         </v-col>
         <v-col
             lg="3"
@@ -94,7 +102,7 @@ const menuSecond: { title: string, link: string }[] = [
             elevation="0"
             rounded="lg"
             color="white"
-            class="pa-7"
+            class="pa-7 h-100 h-lg-auto"
           >
             <div class="text-dark-primary-2 font-weight-medium lh-normal text-body-1 mb-5">
               <p>Остались вопросы?</p>
